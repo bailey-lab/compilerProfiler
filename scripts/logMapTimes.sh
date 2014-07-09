2ed979a202be4716c5e06c1be8c0a51dafd3212a
Nicholas Hathaway <nickjhathaway@gmail.com>
2014-07-09 13:02:50 -0400

    no message

--- scripts/logMapTimes.sh
+++ scripts/logMapTimes.sh
@@ -4,23 +4,23 @@ if [ ! -d profiles ]; then
 	mkdir profiles
 fi 
 runTimes=4
-lens=(100 200 300 400 500 600 700 800 900)
-gxxProfilerNoOpt mapVsUnorderedMap -strNum 100000 -minSize 50 -maxSize 50 -header > profiles/logMapTimes.txt
+lens=(1000 2000 3000 4000 5000 6000 7000 8000 9000 10000)
+gxxProfilerNoOpt mapVsUnorderedMap -strNum 10000 -minSize 50 -maxSize 50 -header > profiles/logMapTimes.txt
 for i in $(seq 1 3); do 
-	gxxProfilerNoOpt mapVsUnorderedMap -strNum 100000 -minSize 50 -maxSize 50 >> profiles/logMapTimes.txt
+	gxxProfilerNoOpt mapVsUnorderedMap -strNum 10000 -minSize 50 -maxSize 50 >> profiles/logMapTimes.txt
 done
 for i in $(seq 1 $runTimes); do 
-	gxxProfiler mapVsUnorderedMap -strNum 100000 -minSize 50 -maxSize 50 >> profiles/logMapTimes.txt
-	clangProfilerNoOpt mapVsUnorderedMap -strNum 100000 -minSize 50 -maxSize 50 >> profiles/logMapTimes.txt
-	clangProfiler mapVsUnorderedMap -strNum 100000 -minSize 50 -maxSize 50 >> profiles/logMapTimes.txt
+	gxxProfiler mapVsUnorderedMap -strNum 10000 -minSize 50 -maxSize 50 >> profiles/logMapTimes.txt
+	clangProfilerNoOpt mapVsUnorderedMap -strNum 10000 -minSize 50 -maxSize 50 >> profiles/logMapTimes.txt
+	clangProfiler mapVsUnorderedMap -strNum 10000 -minSize 50 -maxSize 50 >> profiles/logMapTimes.txt
 done
 
   
 for l in "${lens[@]}" ; do
 	for i in $(seq 1 $runTimes); do 
-		gxxProfilerNoOpt mapVsUnorderedMap -strNum 100000 -minSize $l -maxSize $l >> profiles/logMapTimes.txt
-		gxxProfiler mapVsUnorderedMap -strNum 100000 -minSize $l -maxSize $l >> profiles/logMapTimes.txt
-		clangProfilerNoOpt mapVsUnorderedMap -strNum 100000 -minSize $l -maxSize $l >> profiles/logMapTimes.txt
-		clangProfiler mapVsUnorderedMap -strNum 100000 -minSize $l -maxSize $l >> profiles/logMapTimes.txt
+		gxxProfilerNoOpt mapVsUnorderedMap -strNum 10000 -minSize $l -maxSize $l >> profiles/logMapTimes.txt
+		gxxProfiler mapVsUnorderedMap -strNum 10000 -minSize $l -maxSize $l >> profiles/logMapTimes.txt
+		clangProfilerNoOpt mapVsUnorderedMap -strNum 10000 -minSize $l -maxSize $l >> profiles/logMapTimes.txt
+		clangProfiler mapVsUnorderedMap -strNum 10000 -minSize $l -maxSize $l >> profiles/logMapTimes.txt
 	done
 done
